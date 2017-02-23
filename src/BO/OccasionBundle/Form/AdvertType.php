d3334459435167f943aa2707e6bbf831e3a585a2
yecin <yassine.tanit@gmail.com>
2017-02-23 11:05:37 +0100

    Update AdvertType.php

--- src/BO/OccasionBundle/Form/AdvertType.php
+++ src/BO/OccasionBundle/Form/AdvertType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 //*****************************************************************
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
